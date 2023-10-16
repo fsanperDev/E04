d118389278ebee9c399e182e336a73c187f50e8c
fsanperDev <fsanperdev@gmail.com>
2023-10-16 21:52:20 +0200

    funcionalidades boton, dialog, toast

--- app/src/main/java/com/fsanper/e04/Login.kt
+++ app/src/main/java/com/fsanper/e04/Login.kt
@@ -1,7 +1,9 @@
 package com.fsanper.e04
 
 import android.util.Patterns
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,8 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -31,15 +36,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 
 @Composable
@@ -59,6 +65,13 @@ fun MySpacer(size: Int) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Login() {
+    val context = LocalContext.current
+    val userValido = "prueba@gmail.com"
+    val passValido = "123456"
+    var openForgottenPass = rememberSaveable() {
+        mutableStateOf(false)
+    }
+
     var hidden by remember { mutableStateOf(true) }
     var myColor = Color(0xFF0197FE)
     var username by rememberSaveable() {
@@ -73,6 +86,15 @@ fun Login() {
     val isEmailValid = username.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(username).matches()
     val isPasswordValid = password.length >= 6
     val isButtonEnabled = isEmailValid && isPasswordValid
+
+    Column (modifier = Modifier.padding(bottom = 15.dp, top = 15.dp, end = 16.dp),
+        horizontalAlignment = Alignment.End){
+        Icon(
+            imageVector = Icons.Rounded.Close,
+            contentDescription ="Icon",
+            tint = Color.Black,
+            modifier = Modifier.clickable {  })
+    }
 
     Column(
         modifier = Modifier
@@ -119,18 +141,20 @@ fun Login() {
                         else R.drawable.ic_visibility
                     )
                     val description = if (hidden) "Ocultar contraseña" else "Revelar contraseña" //6
-                    Icon(painter = vector, contentDescription = description,
+                    Icon(
+                        painter = vector, contentDescription = description,
                         tint = Color.Gray,
                         modifier = Modifier
                             .width(25.dp)
                             .height(25.dp)
-                            .padding(end = 5.dp))
+                            .padding(end = 5.dp)
+                    )
                 }
             }
         )
 
         TextButton(
-            onClick = { /*TODO*/ },
+            onClick = { openForgottenPass.value = !openForgottenPass.value },
             modifier = Modifier
                 .align(Alignment.End)
                 .padding(3.dp)
@@ -139,9 +163,23 @@ fun Login() {
                 text = "Forgotten password?", fontSize = 10.sp, color = myColor
             )
         }
+        when{
+            openForgottenPass.value -> {
+                ForgottenPassword(
+                    onDimissRequest = {openForgottenPass.value = false},
+                    onConfirmation = {openForgottenPass.value = false}
+                )
+            }
+        }
 
         Button(
-            onClick = {  },
+            onClick = {
+                if ((username == userValido) && (password == passValido)){
+                    Toast.makeText(context, "Usuario correcto", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(context, "Usuario incorrecto", Toast.LENGTH_SHORT).show()
+                }
+                      },
             colors = ButtonDefaults.buttonColors(
                 containerColor = myColor,
                 contentColor = Color.White
@@ -185,22 +223,73 @@ fun Login() {
             thickness = 1.dp,
         )
         Row {
-            Text(text = "Don't have an account?", modifier = Modifier.padding(top = 30.dp),
-                fontSize = 12.sp)
+            Text(
+                text = "Don't have an account?", modifier = Modifier.padding(top = 30.dp),
+                fontSize = 12.sp
+            )
             TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "Sign Up", color = myColor, fontSize = 12.sp,
-                    modifier = Modifier.padding(top = 22.dp))
+                Text(
+                    text = "Sign Up", color = myColor, fontSize = 12.sp,
+                    modifier = Modifier.padding(top = 22.dp)
+                )
             }
         }
 
     }
 }
 
-fun enableLogin(email: String, password: String, function: () -> Unit): Boolean {
-    if(Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6){
-        return true
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ForgottenPassword(
+    onDimissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+
+    Dialog(onDismissRequest = { onDimissRequest() }) {
+        Card (
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .padding(16.dp)
+        ){
+            Column( modifier = Modifier
+                .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = "Introduce your Email")
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {  },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp, top = 25.dp),
+                    placeholder = { Text(text = "Email address", color = Color.Gray) }
+
+                )
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ){
+                    TextButton(
+                        onClick = { onDimissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Dismiss")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            }
+
+
+        }
     }
-    return false
 }
 
 
