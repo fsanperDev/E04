02b62c731180820b4a2b706dfdaa3194c6a277c6
fsanperDev <fsanperdev@gmail.com>
2023-10-16 14:25:05 +0200

    imagen, textfield, button

--- app/src/main/java/com/fsanper/e04/Login.kt
+++ app/src/main/java/com/fsanper/e04/Login.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -48,9 +49,11 @@ fun Login() {
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Spacer(modifier = Modifier.weight(1f))
+
         Image(
             painter = painterResource(id = R.drawable.insta),
             contentDescription = "Instagram",
@@ -60,8 +63,13 @@ fun Login() {
         )
 
         TextField(value = username, onValueChange = { username = it })
+        Spacer(modifier = Modifier.height(10.dp))
         TextField(value = password, onValueChange = { password = it })
-        Text(text = "Este es un texto de prueba")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Iniciar Sesion")
+        }
 
 
     }
