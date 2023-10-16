02b92899c4fe1d9d785740add2a99c892258e27d
fsanperDev <fsanperdev@gmail.com>
2023-10-16 14:13:27 +0200

    imagen y textfield

--- app/src/main/java/com/fsanper/e04/Login.kt
+++ app/src/main/java/com/fsanper/e04/Login.kt
@@ -5,10 +5,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(
     name = "P1",
     showSystemUi = true,
@@ -28,20 +38,30 @@ import androidx.compose.ui.unit.dp
 )
 @Composable
 fun Login() {
+    var username by rememberSaveable(){
+        mutableStateOf("")
+    }
+    var password by rememberSaveable() {
+        mutableStateOf("")
+    }
     Column(
         modifier = Modifier
-            .height(400.dp)
-            .padding(30.dp)
-            .background(Color.Green),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
+            .padding(16.dp),
+
     ) {
-        Column() {
-            Image(
-                painter = painterResource(id = R.drawable.insta),
-                contentDescription = "Instagram"
-            )
-        }
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.insta),
+            contentDescription = "Instagram",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        TextField(value = username, onValueChange = { username = it })
+        TextField(value = password, onValueChange = { password = it })
+        Text(text = "Este es un texto de prueba")
 
 
     }
